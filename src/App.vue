c4716ad03c01ebc12ae5d1c1ac738415321bd249
Natdanai Kanyakoon <brothermos@Natdanais-MacBook-Air.local>
2022-09-11 21:31:54 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+        <h1>ระบบจัดการข้อมูลพนักงาน</h1>
+    </header>
+    <FormComponent @save="insertEmployee" />
+    <section class="employee-content" v-if="employee.length > 0">
+        <h2>ข้อมูลพนักงาน</h2>
+        <ListData :employee="employee"/>
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListData from "./components/ListData.vue";
+import FormComponent from "./components/FormComponent.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        ListData,
+        FormComponent,
+    },
+    data() {
+        return {
+            employee: [],
+        };
+    },
+    methods: {
+        insertEmployee(data) {
+            this.employee.push(data);
+        },
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+}
+
+header {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 3rem;
+    border-radius: 10px;
+    padding: 1rem;
+    background-color: rgb(116, 116, 198);
+    color: white;
+    text-align: center;
+}
+.employee-content {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 3rem;
+    border-radius: 10px;
+    padding: 1rem;
+    text-align: center;
+}
+.employee-content h2 {
+    font-size: rem;
+    border-bottom: 2px solid #ccc;
+    margin: 0 0 1rem 0;
+    color: rgb(255, 77, 0);
+    padding-bottom: 5px;
 }
 </style>
---
+++ src/components/Person.vue
@@ -0,0 +1,67 @@
+<template>
+    <card>
+        <template v-slot:card-header>
+            <h1>{{ name }}</h1>
+        </template>
+
+        <template v-slot:card-content>
+            <p>เพศ : {{ gender }}</p>
+            <p>เงินเดือน : {{ salary }} บาท,</p>
+            <p>ตำแหน่งงาน: {{ department }}</p>
+            <p>ทักษะด้านภาษา : {{ skill }}</p>
+        </template>
+    </card>
+</template>
+
+<script>
+import Card from "./Card.vue";
+export default {
+    name: "Person",
+    components: {
+        Card,
+    },
+    props: {
+        name: {
+            type: String,
+            requried: true,
+        },
+        salary: {
+            type: Number,
+            default: 15000,
+        },
+        department: {
+            type: String,
+            required: true,
+        },
+        gender: {
+            type: String,
+        },
+        skill: {
+            type: Array,
+        },
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: yellow;
+}
+button {
+    font: inherit;
+    cursor: pointer;
+    border: 1px solid #ff0077;
+    background: #ff0077;
+    color: white;
+    padding: 0.05rem 1rem;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+    margin: 5px;
+    border-radius: 10px;
+}
+.fade-enter-from {
+    opacity: 0;
+}
+.fade-enter-active {
+    transition: all 0.5s linear;
+}
+</style>
